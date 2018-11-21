3afe269bfcb7531a45d133f534935aee37bbf519
geciu <getekf@gmail.com>
2018-11-21 20:48:42 +0100

    Commit message

--- src/wszechswiat/Swiat.java
+++ src/wszechswiat/Swiat.java
@@ -1,10 +1,50 @@
 package wszechswiat;
+import java.util.Scanner;
+
 
 public class Swiat {
 
-    public enum planety{GazowyOlbrzym, SkalistyKarzel, WielkiObjekt, PlanetaWiedzy};
+    public enum typPlanety{GazowyOlbrzym, SkalistyKarzel, WielkiObjekt, PlanetaWiedzy};
     public static void main (String args[]){
 
+        typPlanety[] nowePlanety = new typPlanety[10];
+        nowePlanety[0] = typPlanety.GazowyOlbrzym;
+        nowePlanety[1] = typPlanety.SkalistyKarzel;
+        nowePlanety[2] = typPlanety.WielkiObjekt;
+
+        typPlanety[] innePlanety = new typPlanety[10];
+        innePlanety[0] = typPlanety.GazowyOlbrzym;
+        innePlanety[1] = typPlanety.WielkiObjekt;
+        innePlanety[2] = typPlanety.PlanetaWiedzy;
+
+        Uklad konstelacja = new Uklad("Konstelacja", 15600000, nowePlanety);
+        konstelacja.czasSwiatla();
+        konstelacja.przeszukanie(typPlanety.PlanetaWiedzy);
+        konstelacja.setNazwa("Nowa Konstelacja");
+        konstelacja.wyswietl();
+        System.out.print("\n");
+
+        Uklad mglawica = new Uklad("Mglawica", 678890000, innePlanety);
+        mglawica.czasSwiatla();
+        mglawica.przeszukanie(typPlanety.GazowyOlbrzym);
+        mglawica.setNazwa("Nowa Konstelacja");
+        mglawica.wyswietl();
+        System.out.print("\n");
+
+
+        Uklad nowy = new Uklad(innePlanety);
+        String nowaNazwa;
+        int nowaOdl;
+
+        Scanner odczyt = new Scanner(System.in);
+        System.out.println("Podaj nazwę planety: ");
+        nowaNazwa = odczyt.nextLine();
+        System.out.println("Podaj odległość od Ziemi: ");
+        nowaOdl = odczyt.nextInt();
+
+        nowy.setNazwa(nowaNazwa);
+        nowy.setOdleglosc(nowaOdl);
+        nowy.czasSwiatla();
 
     }
 }
--- src/wszechswiat/Uklad.java
+++ src/wszechswiat/Uklad.java
@@ -1,38 +1,59 @@
 package wszechswiat;
-import wszechswiat.Swiat.planety;
+import wszechswiat.Swiat.typPlanety;
 
 public class Uklad {
 
     private String nazwaGwiazdy;
-    private double odlOdZiemi;
-    private Swiat.planety[] planetyUkladu;
+    private int odlOdZiemi;
+    private Swiat.typPlanety[] planetyUkladu;
 
-    private double predkosc = 299792;
+    private int predkosc = 299792;
 
-    Uklad(String nazwaGwiazdy, double odlOdZiemi, Swiat.planety[] planetyUkladu){
+    Uklad(String nazwaGwiazdy, int odlOdZiemi, Swiat.typPlanety[] planetyUkladu){
         this.nazwaGwiazdy=nazwaGwiazdy;
         this.odlOdZiemi=odlOdZiemi;
         this.planetyUkladu = planetyUkladu;
+        System.out.print("Nazwa: " + nazwaGwiazdy + ", odleglosc od Ziemi: " + odlOdZiemi + "\n");
+
     }
 
-    Uklad(Swiat.planety[] planetyUkladu){
+    Uklad(Swiat.typPlanety[] planetyUkladu){
         this.planetyUkladu = planetyUkladu;
+        System.out.print("\n");
     }
 
 
-    public void czasSwiatla(double odleglosc){
-        double czas;
-        czas=odleglosc/predkosc;
+    public void czasSwiatla(){
+        int czas;
+        czas=odlOdZiemi/predkosc;
         System.out.println("Czas bez kosmitów to " + czas);
         double czasK=czas;
 
-        for(int i=0; i < this.planetyUkladu.length; i++){
-            if(this.planetyUkladu[i] != null){
-                czasK = (czasK + 0.1*czasK);
-            }
+        for(int i=0; i < this.planetyUkladu.length; i++)
+        {
+            if(this.planetyUkladu[i] != null){czasK = (czasK + 0.1*czasK);}
         }
 
 
-        int poczworzony_czas_podrozy = czas << 2;
+        int rozciagnietyCzas = czas << 2;
+        System.out.println("Czas z kosmitami: " + czasK);
+        System.out.println("Czas poczworzony: " + rozciagnietyCzas);
+        System.out.println(" ");
     }
-}mm
+
+    public void przeszukanie(Swiat.typPlanety typPlanety){
+        for(int i=0; i<this.planetyUkladu.length; i++){
+            if(this.planetyUkladu[i].equals(typPlanety)){System.out.println("Uklad " + planetyUkladu + " posiada " + typPlanety);}
+            else if(i == this.planetyUkladu.length - 1) System.out.println(" "/*"Uklad " + planetyUkladu + " nie posiada " + typPlanety*/);
+        }
+
+    }
+
+
+
+    public void setNazwa(String nowaNazwa){this.nazwaGwiazdy=nowaNazwa;}
+
+    public void setOdleglosc(int nowaOdl){this.odlOdZiemi=nowaOdl;}
+
+    public void wyswietl(){System.out.print("Nazwa: " + nazwaGwiazdy + ", odleglosc od Ziemi: " + odlOdZiemi + "\n");}
+}
