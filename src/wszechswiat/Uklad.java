749d3c50626926a7e5914b36198d69923cde6865
geciu <getekf@gmail.com>
2018-11-21 14:41:53 +0100

    Commit message

--- src/wszechswiat/Uklad.java
+++ src/wszechswiat/Uklad.java
@@ -5,19 +5,34 @@ public class Uklad {
 
     private String nazwaGwiazdy;
     private double odlOdZiemi;
-    Swiat.planety planetyUkladu;
+    private Swiat.planety[] planetyUkladu;
 
     private double predkosc = 299792;
 
-    Uklad(String nazwaGwiazdy, double odlOdZiemi, Swiat.planety planetyUkladu){
+    Uklad(String nazwaGwiazdy, double odlOdZiemi, Swiat.planety[] planetyUkladu){
         this.nazwaGwiazdy=nazwaGwiazdy;
         this.odlOdZiemi=odlOdZiemi;
-        this.planetyUkladu=planetyUkladu;
+        this.planetyUkladu = planetyUkladu;
     }
 
-    Uklad(Swiat.planety planetyUkladu){
-        this.planetyUkladu=planetyUkladu;
+    Uklad(Swiat.planety[] planetyUkladu){
+        this.planetyUkladu = planetyUkladu;
     }
 
 
+    public void czasSwiatla(double odleglosc){
+        double czas;
+        czas=odleglosc/predkosc;
+        System.out.println("Czas bez kosmitów to " + czas);
+        double czasK=czas;
+
+        for(int i=0; i < this.planetyUkladu.length; i++){
+            if(this.planetyUkladu[i] != null){
+                czasK = (czasK + 0.1*czasK);
+            }
+        }
+
+
+        int poczworzony_czas_podrozy = czas << 2;
+    }
 }
